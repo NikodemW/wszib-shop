66c13bfed713c7bc9542db7be11c60d85b967723
Nikodem Tadeusz Walczak <nwalczak@student.wszib.edu.pl>
2018-01-13 15:39:40 +0100

    add new model

--- src/Shop.Web/Models/AddProductViewModel.cs
+++ src/Shop.Web/Models/AddProductViewModel.cs
@@ -1,25 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Web.Models
 {
-    public class ProductViewModel
+    public class AddProductViewModel : ProductViewModel
     {
-        
-        [Required(ErrorMessage = "Name was not provided")]
-        [StringLength(100, MinimumLength = 3)]
-        public string Name { get; set; }
-
-        [Required]
-        public string Category { get; set; }
-
-        [Required]
-        [Range(1,100000)]
-        public decimal Price { get; set; }
         public List<SelectListItem> Categories { get; } = new List<SelectListItem>
         {
             new SelectListItem{ Text = "Electronics", Value = "Electronics"},
@@ -27,6 +15,5 @@ namespace Shop.Web.Models
             new SelectListItem{ Text = "Trousers", Value = "Trousers"}
 
         };
-        public Guid Id { get; internal set; }
     }
 }
