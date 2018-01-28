6abb87b8bd73c150718d1b3260d756fcfd0454b9
Nikodem Tadeusz Walczak <nwalczak@student.wszib.edu.pl>
2018-01-28 09:46:14 +0100

    Add test_v2

--- tests/Shop.Tests/Reopsitories/OrderRepositoryTests.cs
+++ tests/Shop.Tests/Reopsitories/OrderRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using Shop.Core.Domain;
+﻿using AutoFixture;
+using FluentAssertions;
+using Shop.Core.Domain;
 using Shop.Core.Repositories;
 using System;
 using System.Collections.Generic;
@@ -18,7 +20,9 @@ namespace Shop.Tests.Reopsitories
             var cart = new Cart();
             cart.AddProduct(new Product("test", "test", 1));
             var order = new Order(new User("test", "test"), cart);
-            
+
+            //Stub - klasa tworząca dane testowe
+
             //Act
             orderRepository.Add(order);
 
@@ -30,6 +34,36 @@ namespace Shop.Tests.Reopsitories
             Assert.NotEmpty(orders);
             Assert.Single(orders);
             Assert.Contains(orders, o => o.Id == order.Id);
+        }
+
+
+        [Fact]
+        public void adding_an_order_should_succeed_v2()
+        {
+            //Arrange
+            IOrderRepository orderRepository = new OrderRepository();
+            var fixture = new Fixture();
+            var cart = fixture.Create<Cart>();
+            var product = fixture.Create<Product>();
+            var user = fixture.Create<User>();
+            
+            cart.AddProduct(product);
+            var order = new Order(user, cart);
+
+            //Stub - klasa tworząca dane testowe
+
+            //Act
+            orderRepository.Add(order);
+
+            //Assert
+            var expectedOrder = orderRepository.Get(order.Id);
+            expectedOrder.ShouldBeEquivalentTo(order);
+           
+
+            var orders = orderRepository.Browse(order.UserId);
+            orders.Should().NotBeEmpty();
+            orders.Should().ContainSingle();
+            orders.Should().Contain(o => o.Id == order.Id);
 
         }
     }
